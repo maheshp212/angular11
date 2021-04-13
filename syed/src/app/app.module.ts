610618547732fa2dd6a8a7952a271140d0380b13
Mahesh Perla <maheshp212@gmail.com>
2021-04-13 08:58:04 +0530

    first commit

---
+++ syed/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { VariablesComponent } from './variables/variables.component';
+import { IntroComponent } from './intro/intro.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    VariablesComponent,
+    IntroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ syed/src/app/variables/variables.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-variables',
+  templateUrl: './variables.component.html',
+  styleUrls: ['./variables.component.scss']
+})
+export class VariablesComponent implements OnInit {
+
+  fname:string = 'syed';
+  age:number = 40;
+  fruits:string[] = ['orange', 'mango', 'munjakaylu'];
+  user:any = {tech:'angular', exp: 5};
+  gender:boolean = true;
+  interest: null = null;
+  undf:any;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
