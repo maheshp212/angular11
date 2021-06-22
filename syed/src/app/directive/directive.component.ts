b4722ef353df4bc8c31ab7f1d551e8f272b9eefe
Mahesh Perla <maheshp212@gmail.com>
2021-06-22 08:50:52 +0530

    directives

--- syed/src/app/directive/directive.component.ts
+++ syed/src/app/directive/directive.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectiveComponent implements OnInit {
 
+  fname = "syed";
+  fruits = ['apple', 'mango', 'papaya', 'melon'];
   constructor() { }
 
   ngOnInit(): void {
+
+    this.fruits.forEach(function(x){
+      console.log(x);
+    })
   }
 
 }
